db340183bf83ec8164daea968a724c9836d2af69
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-19 16:04:45 -0500

    update the routine for the bot

--- run.py
+++ run.py
@@ -7,5 +7,8 @@ load_dotenv()
 username = os.environ.get('INSTA_USERNAME')
 password = os.environ.get('INSTA_PASSWORD')
 print(username)
+
+# bot instructions
 bot = InstaBot(username, password)
 bot.login()
+bot.get_followers()
